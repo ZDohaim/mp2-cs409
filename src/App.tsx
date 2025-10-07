bd8fd996d188c31388092b0158c5d5aa7ec828c7
Ziad AlDohaim <ziada2@illinois.edu>
2025-10-07 10:31:05 -0500

    changed to browserrouter

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,17 @@ import "./App.scss";
 import ListView from "./list-view/ListView";
 import DetailsView from "./details-view/DetailsView";
 import GalleryView from "./gallery-view/GalleryView";
-import { HashRouter, Routes, Route, Link } from "react-router-dom";
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Link,
+  BrowserRouter,
+} from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <HashRouter>
+      <BrowserRouter basename="mp2-cs409">
         <div className="navbar">
           <h1>Top TVShows</h1>
           <Link to="/" className="nav-buttons">
@@ -21,7 +27,7 @@ function App() {
           <Route path="/details/:id" element={<DetailsView />} />
           <Route path="/gallery" element={<GalleryView />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </div>
   );
 }
